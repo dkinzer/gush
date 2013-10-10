52965fe2dfe5f8d2be839b696f64daa52840b21e
David Kinzer <dtkinzer@gmail.com>
2013-10-10 01:04:37 +0000

    Add travis yml and fix action hook scripts.
    The idea to to make the action hook script both work on production
    and on the CI.

--- .travis.sh
+++ .travis.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 #
-# This is an OpenShift to Travis environment variable map.
+# This is an OpenShift environment variable mock for testing purposes.
 #
 
-if [ -z "$TRAVIS" ]; then
-  mkdir -p data
+if [ -z "$OPENSHIFT_APP_NAME" ]; then
   OPENSHIFT_REPO_DIR=./
   OPENSHIFT_DATA_DIR=./data
+  OPENSHIFT_APP_NAME='drupal'
+  OPENSHIFT_MYSQL_DB_USERNAME='root'
+  OPENSHIFT_MYSQL_DB_PASSWORD=''
+  OPENSHIFT_MYSQL_DB_HOST='127.0.0.1'
+  OPENSHIFT_MYSQL_DB_PORT='3306'
+  OPENSHIFT_DB_URL="mysql://$OPENSHIFT_MYSQL_DB_USERNAME@127.0.0.1/$OPENSHIFT_APP_NAME"
+  DB_ENCODE='utf8'
 fi
